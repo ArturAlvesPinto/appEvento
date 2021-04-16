d3e325346dce744abd7b330edeaf5544f0f2b1c7
artur.alves <artur.alves@sinqia.com.br>
2021-04-16 09:19:00 -0300

    Inicializacao do projeto

---
+++ src/main/java/com/eventoapp/evento/EventoApplication.java
@@ -0,0 +1,13 @@
+package com.eventoapp.evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoApplication.class, args);
+	}
+
+}
