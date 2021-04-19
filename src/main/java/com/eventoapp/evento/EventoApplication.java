20f6176e1b7d77c13b9c32dcb7a1472672588c70
artur.alves <artur.alves@sinqia.com.br>
2021-04-19 14:15:18 -0300

    Aula2 - Solucionando problema devido a versao do SpringBoot

--- src/main/java/com/eventoapp/evento/EventoApplication.java
+++ src/main/java/com/eventoapp/evento/EventoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.eventoapp.controller"})
+@ComponentScan({"com.eventoapp.controller"}) //Necessário colocar caso a versão do spring seja 2.0
+											// Também foi preciso colocar a dependencia "org.apache.tomcat.embed" no pom.xml	
 public class EventoApplication {
 
 	public static void main(String[] args) {
